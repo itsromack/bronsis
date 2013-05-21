f2b5920406030837cce6737bd40ac7bd1c728505
Romack Natividad <romacknatividad@gmail.com>
2013-05-22 01:34:43 +0800

    also added the menu.php

---
+++ menu.php
@@ -0,0 +1,71 @@
+            <!-- Start: Primary navigation -->
+            <div class="nav-collapse collapse">        
+              <ul class="nav pull-right">
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">LBCA<b class="caret"></b></a>
+                  <ul class="dropdown-menu">                    
+                    <li class="nav-header">ABOUT LBCS</li>
+                    <li><a href="product.html">Vision</a></li>
+                    <li><a href="product.html">Mission</a></li>
+                    <li><a href="product.html">Goal</a></li>
+                    <li class="nav-header">LBCA PHILIPPINES</li>
+                    <li><a href="product.html">LBCA</a></li>
+                    <li><a href="product.html">Pioneers</a></li>
+                    <li><a href="product.html">Awards and Achievements</a></li>
+                    <li><a href="product.html">Company Affiliations</a></li>
+                    <li><a href="product.html">Message to the Public</a></li>
+                    <!-- <li><a href="all_products.html">All products</a></li> -->
+                    <li class="divider"></li>
+                    <li class="nav-header">LEGAL DOCUMENTS</li>
+                    <li><a href="service.html">BFAD</a></li>
+                    <li><a href="service.html">SEC</a></li>
+                    <li><a href="service.html">DOH</a></li>
+                    <li><a href="service.html">DTI</a></li>
+                    <li><a href="service.html">Permit to Operate (LBCA Clinic)</a></li>
+                    <!-- <li><a href="all_services.html">All services</a></li> -->
+                  </ul>                  
+                </li>
+                <li><a href="product.html">Alternative Medicine</a></li>
+                <li><a href="product.html">Food Supplement</a></li>
+                <li><a href="product.html">Health Tips</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Products<b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li class="nav-header">PRODUCTS</li>
+                    <li><a href="product.html">Grapeseed Extract with CoQ10</a></li>
+                    <li><a href="product.html">Alkaline Mineral Capsules</a></li>
+                    <li><a href="product.html">Pure Alkaline Drop (SAM)</a></li>
+                    <li><a href="product.html">Vitamin C</a></li>
+                    <li><a href="product.html">Barley</a></li>
+                    <li><a href="product.html">Premium One</a></li>
+                    <li><a href="product.html">Power Drops</a></li>
+                    <li><a href="product.html">Spiruplus</a></li>
+                    <li><a href="product.html">Pure Natural Raw Honey</a></li>
+                    <li><a href="product.html">Cotton Hankey</a></li>
+                    <li><a href="product.html">Organic Juices</a></li>
+                    <li><a href="product.html">Organic Soap and Shampoo</a></li>
+                    <li><a href="product.html">Organic Fertilizer</a></li>
+                  </ul>                  
+                </li>
+                <!--
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">About<b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="product.html">Vision</a></li>
+                    <li><a href="product.html">Mission</a></li>
+                    <li><a href="product.html">Goal</a></li><li><a href="our_works.html">Our works</a></li>
+                    <li><a href="patnerships.html">Parnerships</a></li>
+                    <li><a href="leadership.html">Leadership</a></li>
+                    <li><a href="news.html">News</a></li>
+                    <li><a href="events.html">Events</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                  </ul>
+                </li>
+                -->
+                <!-- <li><a href="faq.html">FAQ</a></li> -->
+                <li><a href="faq.html">Testimonials</a></li>
+                <li><a href="contact_us.html">Contact us</a></li>
+                <!-- <li><a href="signup.html">Sign up</a></li>
+                <li><a href="signin.html">Sign in</a></li> -->
+              </ul>
+            </div>
