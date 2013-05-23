d0beae79675c0c2b5e1a70d7f48b88686e416f59
Romack Natividad <romacknatividad@gmail.com>
2013-05-24 01:31:04 +0800

    added alternative medicine page

---
+++ alternative_medicine.php
@@ -0,0 +1,95 @@
+<?php
+$page_name = "Alternative Medicine";
+require_once("header.php");
+?>
+    <!-- Start: Main content -->
+    <div class="content">    
+      <div class="container">
+        <!-- Start: Product description -->
+      <airticle class="article">
+        <div class="row bottom-space">
+          <div class="span12">
+            <div class="page-header">
+              <h1><?php echo $page_name?> <small><?php echo $page_name?></small></h1>
+            </div>
+          </div>
+          <div class="span12 center-align">
+            <img src="http://placehold.it/800x300" class="thumbnail product-snap">            
+          </div>
+        </div>
+        <div class="row">
+          <div class="span10 offset1">
+<h4>What is alternative medicine?</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<h4>Why do people need the way of alternative medicine?</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<h4>Approximate number of people who were cured by way of alternative medicine and LBCA?</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<h4>What are the scientific and biblical basis of alternative medicine?</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<h4>What are the advantages and disadvantage of alternative medicine techniques?</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            <div class="span8 offset1">
+              <h3>Fetures of <?php echo $product['name']?></h3>
+              <ul class="features">
+                <li>
+                  <i class="icon-ok "></i>Health Benefit
+                </li>
+                <li>
+                  <i class="icon-ok "></i>Health Benefit
+                </li>
+                <li>
+                  <i class="icon-ok "></i>Health Benefit
+                </li>
+                <li>
+                  <i class="icon-ok "></i>Health Benefit
+                </li>
+                <li>
+                  <i class="icon-ok "></i>Health Benefit
+                </li>
+                <li>
+                  <i class="icon-ok "></i>Health Benefit
+                </li>
+                <li>
+                  <i class="icon-ok "></i>Health Benefit
+                </li>
+                <li>
+                  <i class="icon-ok "></i>Health Benefit
+                </li>                  
+              </ul>
+            </div>                                              
+          </div>      
+        </div>
+        <div class="row">
+          <div class="span10 offset1">
+            <hr>
+            <div class="span3">
+              <p>
+                Ready to start the innovation?
+              </p>
+              <a class="btn btn-large btn-block" href="#">Buy now</a>
+            </div>
+            <div class="span3">
+              <p>
+                Got confused?
+              </p>
+              <a class="btn btn-large btn-block" href="contact_us.html">Contact us</a>
+            </div>
+            <div class="span3">
+              <p>
+                Check frequently asked question
+              </p>
+              <a class="btn btn-large btn-block" href="faq.html">FAQ</a>
+            </div>              
+          </div>
+        </div>
+      </airticle>
+      <!-- End: Product description -->
+      </div>
+    </div>
+    </div>
+    <!-- End: Main content -->
+<?php
+require_once("footer.php");
+?>
