0b84b01717848a9ebb91daeb558c21de1af5e035
Romack Natividad <romacknatividad@gmail.com>
2013-05-26 16:50:30 +0800

    added images for ms.arlyn gayon

--- testimonials.php
+++ testimonials.php
@@ -16,8 +16,8 @@ require_once("header.php");
                 </div>
                 <div class="row">
                     <div class="span10 offset1">
-                        <h3><a name="testimonial1">Juan dela Cruz</a></h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <h3><a name="testimonial1">Ms. Arlyn Gayon</a></h3>
+                        <p><strong>35 years old</strong>. Brgy. Cupang, Antipolo City.</p>
 
                         <h3><a name="testimonial2">Pedro Penduko</a></h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
