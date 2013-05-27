43003715de3fa81c80e50e6a9b3eac345a9674d7
Romack Natividad <romacknatividad@gmail.com>
2013-05-28 06:12:49 +0800

    added images for events on slider

--- index_slider.php
+++ index_slider.php
@@ -8,7 +8,10 @@
                   <div class="row-fluid">
                     <div class="span7 marketting-info">
                       <h1>LIVE BLOOD CELLS ANALYSIS.</h1>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                      <img src="http://placehold.it/200x200" class="thumbnail product-snap">
+                      <img src="http://placehold.it/200x200" class="thumbnail product-snap">
+                      <img src="http://placehold.it/200x200" class="thumbnail product-snap">
+                      <p>The truth about your health is in your blood. Examine your blood now and be knowledgable on how can you and your family stay healthy.</p>
                       <h3>
                         <a href="lbca.php" class="btn btn-primary">Learn more</a>
                       </h3>                      
@@ -23,14 +26,15 @@
                 <div class="hero-unit">
                   <div class="row-fluid">
                     <div class="span7 marketting-info">
-                      <h1>EVENTS: LBCA IN ACTION</h1>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                      <h1>LBCA IN ACTION</h1>
+                      <img src="img/lbca_balagtas1.jpg" class="thumbnail product-snap">
+                      <p>It is our commitment to spread love and make the people know the truth. We want your family to be healthy and happy.</p>
                       <h3>
                         <a href="events.php" class="btn btn-primary">Learn more</a>
                       </h3>                      
                     </div>
                     <div class="span5">
-                      <img src="img/placeholder.jpg" class="thumbnail">
+                      <img src="img/lbca_apalit1.jpg" class="thumbnail">
                     </div>
                   </div>                  
                 </div>
@@ -40,6 +44,9 @@
                   <div class="row-fluid">
                     <div class="span7 marketting-info">
                       <h1>LBCA PHILIPPINES COMPANY PRESIDENT</h1>
+                      <img src="http://placehold.it/200x200" class="thumbnail product-snap">
+                      <img src="http://placehold.it/200x200" class="thumbnail product-snap">
+                      <img src="http://placehold.it/200x200" class="thumbnail product-snap">
                       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                       <h3>
                         <a href="jb.php" class="btn btn-primary">Learn more</a>
@@ -56,7 +63,10 @@
                   <div class="row-fluid">
                     <div class="span7 marketting-info">
                       <h1>PATIENTS WHO ARE TREATED AND CURED</h1>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                      <img src="http://placehold.it/200x200" class="thumbnail product-snap">
+                      <img src="http://placehold.it/200x200" class="thumbnail product-snap">
+                      <img src="http://placehold.it/200x200" class="thumbnail product-snap">
+                      <p>Read and watch amazing testimonies from patients who undergone live blood cell analysis. Read and hear their inspiring stories on how they felt better and cured.</p>
                       <h3>
                         <a href="testimonials.php" class="btn btn-primary">Learn more</a>
                       </h3>                      
