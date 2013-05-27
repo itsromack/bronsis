6649cdd45716e25737260ec95d309453f634a83e
Romack Natividad <romacknatividad@gmail.com>
2013-05-28 05:20:28 +0800

    updated index_slider

--- index_slider.php
+++ index_slider.php
@@ -10,11 +10,11 @@
                       <h1>LIVE BLOOD CELLS ANALYSIS.</h1>
                       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                       <h3>
-                        <a href="product.html" class="btn btn-primary">Learn more</a>
+                        <a href="lbca.php" class="btn btn-primary">Learn more</a>
                       </h3>                      
                     </div>
                     <div class="span5">
-                      <img src="img/placeholder.jpg" class="thumbnail">
+                      <img src="img/rbc470.JPG" class="thumbnail">
                     </div>
                   </div>                  
                 </div>
@@ -26,7 +26,7 @@
                       <h1>EVENTS: LBCA IN ACTION</h1>
                       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                       <h3>
-                        <a href="service.html" class="btn btn-primary">Learn more</a>
+                        <a href="events.php" class="btn btn-primary">Learn more</a>
                       </h3>                      
                     </div>
                     <div class="span5">
@@ -42,11 +42,11 @@
                       <h1>LBCA PHILIPPINES COMPANY PRESIDENT</h1>
                       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                       <h3>
-                        <a href="product.html" class="btn btn-primary">Learn more</a>
+                        <a href="jb.php" class="btn btn-primary">Learn more</a>
                       </h3>                      
                     </div>
                     <div class="span5">
-                      <img src="img/placeholder.jpg" class="thumbnail">
+                      <img src="img/jb1_editted.JPG" class="thumbnail">
                     </div>
                   </div>                  
                 </div>
@@ -58,7 +58,7 @@
                       <h1>PATIENTS WHO ARE TREATED AND CURED</h1>
                       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                       <h3>
-                        <a href="product.html" class="btn btn-primary">Learn more</a>
+                        <a href="testimonials.php" class="btn btn-primary">Learn more</a>
                       </h3>                      
                     </div>
                     <div class="span5">
