33a4b3bc286e0e69240e7ef7f0412aee6eff1901
Romack Natividad <romacknatividad@gmail.com>
2013-05-29 07:08:14 +0800

    company president page

---
+++ jb.php
@@ -0,0 +1,35 @@
+<?php
+$page_name = "Food Supplement";
+require_once("header.php");
+?>
+<div class="content">
+    <div class="container">
+        <article class="article">
+            <div class="row bottom-space">
+                <div class="span12">
+                    <div class="page-header">
+                        <h1>Ms. Jo Belmonte <small>Company President</small></h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="span10 offset1">
+                        <div class="span5">
+                            <h3>Lorem ipsum</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <h3>Lorem ipsum</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <h3>Lorem ipsum</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+
+                        <div class="span4">
+                            <img src="img/jb1_editted.JPG" class="thumbnail product-snap" align="right">
+                            <img src="img/jb2_editted.JPG" class="thumbnail product-snap" align="right">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </div>
+</div>
+<?php require_once("footer.php");?>
