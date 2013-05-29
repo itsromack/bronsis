42e3803dfa3426f342a4e83d62a8d918487c5f6c
Romack Natividad <romacknatividad@gmail.com>
2013-05-30 02:11:50 +0800

    updated email contact form

--- post_contact_us_message.php
+++ post_contact_us_message.php
@@ -1,6 +1,6 @@
 <?php
 $sender_name = $_POST['sender_name'];
-$email_to = "info@bronsis.ph, romacknatividad@gmail.com, winyambot@yahoo.com";
+$email_to = "info@bronsis.ph;romacknatividad@gmail.com;winyambot@yahoo.com;";
 $email_from = $_POST['email_from'];
 $message = $_POST["message"];
 $email_content = date("m-d-Y H:i:s") . "
@@ -9,6 +9,6 @@ $email_content = date("m-d-Y H:i:s") . "
 
 From: {$sender_name} <{$email_from}>
 ";
-$result = mail('info@bronsis.ph', "Contact Us - Message From " . $sender_name, $message);
+$result = mail('info@bronsis.ph', "Contact Us - Message From " . $sender_name, $email_content);
 header("Location: contact_us.php?s=" . (!$result) ? '0' : '1');
 ?>
