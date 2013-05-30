60f620673aadcd734dffce22724b6cd06504f6f4
Elmer Alvarado <elmer.alvarado555@gmail.com>
2013-05-30 22:26:28 +0800

    added location page

---
+++ locations.php
@@ -0,0 +1,244 @@
+<?php
+$page_name = "Locations";
+include_once("header.php");
+?>
+    <!-- Start: MAIN CONTENT -->
+    <div class="content">
+      <!-- Start: LOCATION LIST -->
+        <div class="container">
+          <div class="page-header">
+            <h2>Our Locations</h2>
+          </div>
+          <div class="row-fluid">
+            <ul class="thumbnails">
+              <li class="span3">
+                <div class="thumbnail">
+                  <img src="img/placeholder-260x150.jpg" alt="product name">
+                  <div class="caption">
+                    <h3>Apalit Pampanga</h3>
+                    <p>
+                    </p>
+                  </div>
+                  <div class="widget-footer">
+                    <p>
+                      <center><a href="#" class="btn btn-primary">Patient's List &raquo;</a></center>
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li class="span3">
+                <div class="thumbnail">
+                  <img src="img/placeholder-260x150.jpg" alt="product name">
+                  <div class="caption">
+                    <h3>Dagupan City</h3>
+                    <p>
+                    </p>
+                  </div>
+                  <div class="widget-footer">
+                    <p>
+                      <center><a href="#" class="btn btn-primary">Patient's List &raquo;</a></center>
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li class="span3">
+                <div class="thumbnail">
+                  <img src="img/placeholder-260x150.jpg" alt="product name">
+                  <div class="caption">
+                    <h3>Angeles City</h3>
+                    <p>
+                    </p>
+                  </div>
+                  <div class="widget-footer">
+                    <p>
+                      <center><a href="#" class="btn btn-primary">Patient's List &raquo;</a></center>
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li class="span3">
+                <div class="thumbnail">
+                  <img src="img/placeholder-260x150.jpg" alt="product name">
+                  <div class="caption">
+                    <h3>Cabanatuan City</h3>
+                    <p>
+                    </p>
+                  </div>
+                  <div class="widget-footer">
+                    <p>
+                      <center><a href="#" class="btn btn-primary">Patient's List &raquo;</a></center>
+                    </p>
+                  </div>
+                </div>
+              </li>
+            </ul>
+            <ul class="thumbnails">
+              <li class="span3">
+                <div class="thumbnail">
+                  <img src="img/placeholder-260x150.jpg" alt="product name">
+                  <div class="caption">
+                    <h3>Balagtas Bulacan</h3>
+                    <p>
+                    </p>
+                  </div>
+                  <div class="widget-footer">
+                    <p>
+                      <center><a href="#" class="btn btn-primary">Patient's List &raquo;</a></center>
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li class="span3">
+                <div class="thumbnail">
+                  <img src="img/placeholder-260x150.jpg" alt="product name">
+                  <div class="caption">
+                    <h3>Morong Bataan</h3>
+                    <p>
+                    </p>
+                  </div>
+                  <div class="widget-footer">
+                    <p>
+                      <center><a href="#" class="btn btn-primary">Patient's List &raquo;</a></center>
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li class="span3">
+                <div class="thumbnail">
+                  <img src="img/placeholder-260x150.jpg" alt="product name">
+                  <div class="caption">
+                    <h3>San Pedro Laguna</h3>
+                    <p>
+                    </p>
+                  </div>
+                  <div class="widget-footer">
+                    <p>
+                      <center><a href="#" class="btn btn-primary">Patient's List &raquo;</a></center>
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li class="span3">
+                <div class="thumbnail">
+                  <img src="img/placeholder-260x150.jpg" alt="product name">
+                  <div class="caption">
+                    <h3>Baguio City</h3>
+                    <p>
+                    </p>
+                  </div>
+                  <div class="widget-footer">
+                    <p>
+                      <center><a href="#" class="btn btn-primary">Patient's List &raquo;</a></center>
+                    </p>
+                  </div>
+                </div>
+              </li>
+            </ul>
+            <ul class="thumbnails">
+              <li class="span3">
+                <div class="thumbnail">
+                  <img src="img/placeholder-260x150.jpg" alt="product name">
+                  <div class="caption">
+                    <h3>Sto.Nino, Meycauayan <small><br /><b style = "color:red;">(UPCOMING THIS: JUNE 2)</b></small></h3>
+                    <p>
+                    </p>
+                  </div>
+                  <div class="widget-footer">
+                    <p>
+                      <center><a href="#" class="btn btn-primary">Patient's List &raquo;</a></center>
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li class="span3">
+                <div class="thumbnail">
+                  <img src="img/placeholder-260x150.jpg" alt="product name">
+                  <div class="caption">
+                    <h3>Anjene, Meycauayan <small><br /><b style = "color:red;">(UPCOMING THIS: JUNE 3)</b></small></h3>
+                    <p>
+                    </p>
+                  </div>
+                  <div class="widget-footer">
+                    <?if(date('Y-m-d') != '2013-06-13'):?>
+                    <p>
+                      <center><button class="btn btn-primary" disabled="active">Patient's List &raquo;</button></center>
+                    </p>
+                    <?else:?>
+                    <p>
+                      <center><a href="#" class="btn btn-primary">Patient's List &raquo;</a></center>
+                    </p>
+                    <?endif?>
+                  </div>
+                </div>
+              </li>
+              <li class="span3">
+                <div class="thumbnail">
+                  <img src="img/placeholder-260x150.jpg" alt="product name">
+                  <div class="caption">
+                    <h3>Dolores, <br />San Fernando City <small><br /><b style = "color:red;">(UPCOMING THIS: June 9, 8:00 AM)</b></small></h3>
+                    <p>
+                    </p>
+                  </div>
+                  <div class="widget-footer">
+                    <p>
+                      <center><a href="#" class="btn btn-primary">Patient's List &raquo;</a></center>
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li class="span3">
+                <div class="thumbnail">
+                  <img src="img/placeholder-260x150.jpg" alt="product name">
+                  <div class="caption">
+                    <h3>Cabanatuan City</h3>
+                    <p>
+                    </p>
+                  </div>
+                  <div class="widget-footer">
+                    <p>
+                      <center><a href="#" class="btn btn-primary">Patient's List &raquo;</a></center>
+                    </p>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          <!-- 
+          <div class="pagination pagination-centered">
+                <ul>
+                  <li class="disabled">
+                    <a href="#">&laquo;</a>
+                  </li>
+                  <li class="active">
+                    <a href="#">1</a>
+                  </li>
+                  <li>
+                    <a href="#">2</a>
+                  </li>
+                  <li>
+                    <a href="#">3</a>
+                  </li>
+                  <li>
+                    <a href="#">4</a>
+                  </li>
+                  <li>
+                    <a href="#">5</a>
+                  </li>
+                  <li>
+                    <a href="#">6</a>
+                  </li>
+                  <li>
+                    <a href="#">&raquo;</a>
+                  </li>
+                </ul>
+          </div>
+          -->
+        </div>
+      <!-- End: LOCATION LIST -->
+
+    </div>
+    <!-- End: MAIN CONTENT -->
+<?php
+require_once("footer.php");
+?>
